2011a5632fb0a5c0f72e7fe1f21c38ed040378af
Mohamed-Amine-Slama <143910977+Mohamed-Amine-Slama@users.noreply.github.com>
2026-06-01 21:39:31 +0100

    build(ui): update asset compression plugin options [299/500]

---
+++ src/ui/components.py
@@ -0,0 +1,7 @@
+# UI Module
+
+# [2026-08-06 21:25:29] BUILD (ui): Updated module functionality
+def handle_ui_action_299():
+    """build(ui): update asset compression plugin options"""
+    config = {'scope': 'ui', 'index': 299, 'active': True}
+    return config
